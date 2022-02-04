8f1b9786bbc21393b91b8017434fa9607b2805c6
Gabriel Small <gsmall30@stuy.edu>
2022-02-03 22:30:36 -0500

    made teens more elegant

--- 01-recursion/Challenge.java
+++ 01-recursion/Challenge.java
@@ -1,15 +1,13 @@
 public class Challenge{
   public static void main(String[] args){
-    System.out.println(toWords(94));
+    System.out.println(toWords(13));
   }
   public static String toWords(int num){
     String[] nums = {"zero","one","two","three","four","five","six","seven","eight","nine","ten","","twen","thir","four","fif","six","seven","eight","nine"};
     if(num >= 1000) return nums[(num / 1000)] + " thousand "+toWords(num-(num/1000)*1000);
     else  if(num >= 100) return nums[(num / 100)] + " hundred and "+toWords(num-(num/100)*100);
     else    if(num >= 20) return nums[(num / 10)+10] + "ty-" +toWords(num-(num/10)*10);
-    else    if(num == 15) return "fifteen ";
-    else    if(num >= 14) return nums[(num % 10)] + "teen ";
-    else    if(num == 13) return "thirteen ";
+    else    if(num >= 12) return nums[(num % 10)+10] + "teen ";
     else    if(num == 12) return "twelve ";
     else    if(num == 11) return "eleven ";
     else    if(num >= 1) return nums[(num)];
