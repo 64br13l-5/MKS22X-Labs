6bc349361a81f73623aad0ad5b5abc4d3e27693c
Gabriel Small <gsmall30@stuy.edu>
2022-02-07 11:49:15 -0500

    realized my hundreds are broken

--- 01-recursion/Challenge.java
+++ 01-recursion/Challenge.java
@@ -1,15 +1,19 @@
 public class Challenge{
   public static void main(String[] args){
-    System.out.println(toWords(13));
+    int x = 0;
+    for(int i = 1; i<=1000; i++){
+     x += toWords(i).length();
+    }
+    System.out.println(x);
   }
   public static String toWords(int num){
     String[] nums = {"zero","one","two","three","four","five","six","seven","eight","nine","ten","","twen","thir","four","fif","six","seven","eight","nine"};
-    if(num >= 1000) return nums[(num / 1000)] + " thousand "+toWords(num-(num/1000)*1000);
-    else  if(num >= 100) return nums[(num / 100)] + " hundred and "+toWords(num-(num/100)*100);
-    else    if(num >= 20) return nums[(num / 10)+10] + "ty-" +toWords(num-(num/10)*10);
-    else    if(num >= 12) return nums[(num % 10)+10] + "teen ";
-    else    if(num == 12) return "twelve ";
-    else    if(num == 11) return "eleven ";
+    if(num >= 1000) return nums[(num / 1000)] + "thousand"+toWords(num-(num/1000)*1000);
+    else  if(num >= 100) return nums[(num / 100)] + "hundredand"+toWords(num-(num/100)*100);
+    else    if(num >= 20) return nums[(num / 10)+10] + "ty" +toWords(num-(num/10)*10);
+    else    if(num >= 12) return nums[(num % 10)+10] + "teen";
+    else    if(num == 12) return "twelve";
+    else    if(num == 11) return "eleven";
     else    if(num >= 1) return nums[(num)];
     return "";
   }
