b8eec31dd38549339152d71f9b25097d05b6d62f
Gabriel Small <gsmall30@stuy.edu>
2022-02-08 16:59:19 -0500

    tested some more

--- Classwork/PartialSum.java
+++ Classwork/PartialSum.java
@@ -2,6 +2,13 @@ public class PartialSum{
   public static void main(String[]   args){
     int[] nums = {1,3,2,267};
       System.out.println(partialSum(0,nums,3));
+      int[] nums2 = {2,4,8};
+      System.out.println(partialSum(0,nums2,10));
+      System.out.println(partialSum(0,nums2,14));
+      System.out.println(partialSum(0,nums2,9));
+      int[] nums3 = {2,3,7,10};
+      System.out.println(partialSum(0,nums3,9));
+      
   }
 
   public static boolean partialSum(int start,int[] arr,int targetValue){
