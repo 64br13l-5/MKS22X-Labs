12cde5a46f6d4d33126731748969cce5ba763820
Gabriel Small <gsmall30@stuy.edu>
2022-02-09 11:23:25 -0500

    RecursionClasswork when I tried adding groupsum, didnt work for arr with length 0 or 1, so I did a fix

--- Classwork/Recursion4Real.java
+++ Classwork/Recursion4Real.java
@@ -4,6 +4,8 @@ public  class Recursion4Real{
   }
   public static boolean groupSum(int start, int[] nums, int target) {
   //just PartialSum
+  if(nums.length == 0) return target == 0;
+  if(nums.length == 1) return target == 0 || target == nums[0];
   if(target-nums[start] == 0 )return true;
   if(start == nums.length-1)return false;
   return (groupSum(start+1,nums,target-nums[start]) ||  groupSum(start+1,nums,target));
