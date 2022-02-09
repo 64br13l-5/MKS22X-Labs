152179cd5c337f51241e37f8cd98f2b81703838a
Gabriel Small <gsmall30@stuy.edu>
2022-02-09 11:48:02 -0500

    Recursionclasswork first attempt at group6

--- Classwork/Recursion4Real.java
+++ Classwork/Recursion4Real.java
@@ -13,11 +13,15 @@ public  class Recursion4Real{
 }
 public static boolean groupSum6(int start, int[] nums, int target) {
 //just PartialSum
+groupSum6(start,nums,target,false)
+}
+public static boolean groupSum6(int start, int[] nums, int target,boolean has6) {
+//just PartialSum
 if(nums.length == 0) return target == 0;
 if(nums.length == 1) return target == 0 || target == nums[0];
-if(target-nums[start] == 0 )return true;
+if(target-nums[start] == 0 && has6)return true;
 if(start == nums.length-1)return false;
-return (groupSum6(start+1,nums,target-nums[start]) ||  groupSum6(start+1,nums,target));
+return (groupSum6(start+1,nums,target-nums[start],nums[start]==6) ||  groupSum6(start+1,nums,target,nums[start]==6));
 }
 public static boolean splitArray(int[] nums) {
   return splitArray(0,0,0,nums);
