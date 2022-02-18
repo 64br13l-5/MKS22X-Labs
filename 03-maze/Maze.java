d0505083d671ece9d1c9b4794fcb61aff9d47613
Gabriel Small <gsmall30@stuy.edu>
2022-02-18 11:35:22 -0500

    added text to char array

---
+++ 03-maze/Maze.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+public class Maze{
+  public static char[][] getMaze(String file){
+    String output = "";
+    int l = 0;
+    int w = 0;
+    try {
+                File maze = new File(file);
+                Scanner input = new Scanner(maze);
+                while (input.hasNextLine()) {
+                  String next = input.nextLine();
+                   l = next.length();
+                   w++;
+                   output += next;
+                }
+                input.close();//releases the file from your program
+              } catch (FileNotFoundException ex) {
+                output = "File not Found!";
+              }
+    char[][] ret = new char[w][l];
+    for(int i = 0; i < ret.length;i++){
+      for(int j = 0; j < ret[i].length;j++){
+        ret[i][j] = output.charAt(i*l+j);
+      }
+    }
+    return ret;
+  }
+  public static String charprint(char[][] ret){
+    String output = "";
+    for(int i = 0; i < ret.length;i++){
+      for(int j = 0; j < ret[i].length;j++){
+        output += ret[i][j];
+      }
+      output += "\n";
+    }
+    return output;
+  }
+  public static void main(String[] args){
+    System.out.println(charprint(getMaze("Maze1.txt")));
+  }
+}
