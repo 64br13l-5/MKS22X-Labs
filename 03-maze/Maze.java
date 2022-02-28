995e32e5ef532b50e1958e3e4c694240759928d6
Gabriel Small <gsmall30@stuy.edu>
2022-02-28 11:48:23 -0500

    started Maze.java

--- 03-maze/Maze.java
+++ 03-maze/Maze.java
@@ -1,11 +1,14 @@
 import java.io.*;
 import java.util.*;
 public class Maze{
-  public static char[][] getMaze(String file){
+  private char[][]maze;
+  private boolean animate;//false by default
+  private int startRow,startCol;
+  public char[][] getMaze(String file) {
     String output = "";
     int l = 0;
     int w = 0;
-    try {
+try{
                 File maze = new File(file);
                 Scanner input = new Scanner(maze);
                 while (input.hasNextLine()) {
@@ -15,27 +18,76 @@ public class Maze{
                    output += next;
                 }
                 input.close();//releases the file from your program
-              } catch (FileNotFoundException ex) {
-                output = "File not Found!";
+              }
+              catch (FileNotFoundException e){
+                e.printStackTrace();
               }
     char[][] ret = new char[w][l];
     for(int i = 0; i < ret.length;i++){
       String str = output.substring(i*l,(i+1)*l);
+      if(str.contains("S")){
+        startCol = str.indexOf("S") ;
+        startRow = i;
+      }
       ret[i] = str.toCharArray();
     }
     return ret;
   }
-  public static String charprint(char[][] ret){
+  public void setAnimate(boolean b){
+              animate = b;
+  }
+  private void wait(int millis){
+              try {
+                Thread.sleep(millis);
+              }
+              catch (InterruptedException e) {
+              }
+            }
+  public Maze(String filename){
+    maze = getMaze(filename);
+  }
+  public static void clearTerminal(){
+             //erase terminal
+             System.out.println("\033[2J");
+           }
+  public static void gotoTop(){
+             //go to top left of screen
+             System.out.println("\033[1;1H");
+           }
+  public int solve(){
+              //only clear the terminal if you are running animation
+              if(animate){
+                clearTerminal();
+              }
+              //start solving at the location of the s.
+              return solve(startRow,startCol);
+
+  }
+  private int solve(int row, int col){ //you can add more parameters since this is private
+              //automatic animation! You are welcome.
+              if(animate){
+                gotoTop();
+                System.out.println(this);
+                wait(50);
+              }
+
+              //COMPLETE SOLVE
+              return 0; //so it compiles
+  }
+  public  String toString(){
     String output = "";
-    for(int i = 0; i < ret.length;i++){
-      for(int j = 0; j < ret[i].length;j++){
-        output += ret[i][j];
+    for(int i = 0; i < maze.length;i++){
+      for(int j = 0; j < maze[i].length;j++){
+        output += maze[i][j];
       }
       output += "\n";
     }
     return output;
   }
   public static void main(String[] args){
-    System.out.println(charprint(getMaze("Maze1.txt")));
+
+    Maze a = new Maze("Maze1.txt");
+    System.out.println(a);
+
   }
 }
