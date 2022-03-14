08b7b030df8ac338fd6e409d3d3eb2e249b34526
Gabriel Small <gsmall30@stuy.edu>
2022-03-14 11:48:03 -0400

    started pivot

---
+++ 05-quick/Preliminary.java
@@ -0,0 +1,11 @@
+public class Preliminary{
+  public static int partition ( int [] data, int start, int end){
+    int pivot = start;
+    for(int i = start; i <end ; i++){
+      if(data[i] < data[pivot]) pivot ++; 
+      else{
+
+      }
+    }
+}
+}
