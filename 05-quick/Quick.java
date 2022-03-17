4f13cf339c1b870c4aeedc33cb66eee99d89d492
Gabriel Small <gsmall30@stuy.edu>
2022-03-17 19:17:43 -0400

    Fixed Dutch and made code more elegant for partitions

--- 05-quick/Quick.java
+++ 05-quick/Quick.java
@@ -1,40 +1,53 @@
 import java.util.*;
 public class Quick{
-  public static void swap(int[] data, int a, int b){
+  private static void swap(int[] data, int a, int b){
     int val = data[a];
     data[a] = data[b];
     data[b] = val;
   }
-  public static int partition ( int [] data, int start, int end){
-    if(end == start) return end;
-    Random rng = new Random();
-    int pivot = start + rng.nextInt(end-start);
-    int val = data[pivot];
-    swap(data,pivot,start);
-    pivot = start;
-    int i = start+1;
-    while (i<=end){
-      if(data[i] < val) {
-        i++;pivot++;
+  private static   Random rng = new Random();
+  public static int partitionDutch ( int [] data, int lo, int hi){
+    if(hi == lo) return hi;
+    int c = 0;
+    swap(data,(lo + rng.nextInt(hi-lo)),lo);
+    while (lo+1<=hi){
+      if(data[lo+1] < data[lo-c]) {
+        swap(data,lo-c,lo+1);
+        lo++;
       }
-      else{
-        int x = data[i];
-        data[i] = data[end];
-        data[end]=x;
-        end--;
+      else if(data[lo+1] > data[lo]){
+        swap(data,lo+1,hi);
+        hi--;
+      }else{
+        lo++;c++;
       }
     }
-    swap(data,pivot,start);
-    return pivot;
+    return lo-c;
+}
+  public static int partition ( int [] data, int lo, int hi){
+    if(hi == lo) return hi;
+    int st = lo;
+    swap(data,(lo + rng.nextInt(hi-lo)),lo);
+    while (lo<hi){
+      if(data[lo+1] < data[st]) {
+        lo++;
+      }
+      else{
+        swap(data,lo+1,hi);
+        hi--;
+      }
+    }
+    swap(data,lo,st);
+    return lo;
 }
 public static void quicksort(int[] data){
-  int start = 0;
-  int end = data.length-1;
-  quicksort(data,start,end);
+  int lo = 0;
+  int hi = data.length-1;
+  quicksort(data,lo,hi);
 }
 public static void quicksort(int[] data, int s, int e){
   if(s >= e) return;
-  int i = partition(data,s,e);
+  int i = partitionDutch(data,s,e);
   quicksort(data,i+1,e);
   quicksort(data,s,i);
 }
@@ -50,13 +63,13 @@ public static int[] arrgen(int len){
 *@param k is 0 to data.length-1 inclusive
 *@postcondition The array may be modified. */
 public static int quickselect(int []data, int k){
-  int start = 0;
-  int end = data.length-1;
+  int lo = 0;
+  int hi = data.length-1;
   while(true){
-    int i = partition(data,start,end);
+    int i = partitionDutch(data,lo,hi);
   if(k == i) return data[k];
-  if(k < i) end = i-1;
-  else start = i+1;
+  if(k < i) hi = i-1;
+  else lo = i+1;
 }
 }
 public static void main(String[] args){
