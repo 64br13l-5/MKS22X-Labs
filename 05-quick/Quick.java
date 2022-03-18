203d3c1a885277e8ce8b9d742e48c1ad463b8244
Gabriel Small <gsmall30@stuy.edu>
2022-03-17 20:11:28 -0400

    made partition more clean and awesome and elegant

--- 05-quick/Quick.java
+++ 05-quick/Quick.java
@@ -7,7 +7,6 @@ public class Quick{
   }
   private static   Random rng = new Random();
   public static int partitionDutch ( int [] data, int lo, int hi){
-    if(hi == lo) return hi;
     int c = 0;
     swap(data,(lo + rng.nextInt(hi-lo)),lo);
     while (lo+1<=hi){
@@ -24,21 +23,14 @@ public class Quick{
     }
     return lo-c;
 }
-  public static int partition ( int [] data, int lo, int hi){
-    if(hi == lo) return hi;
-    int st = lo;
-    swap(data,(lo + rng.nextInt(hi-lo)),lo);
-    while (lo<hi){
-      if(data[lo+1] < data[st]) {
-        lo++;
-      }
-      else{
-        swap(data,lo+1,hi);
-        hi--;
-      }
-    }
-    swap(data,lo,st);
-    return lo;
+public static int partition ( int [] data, int lo, int hi){
+int st = lo;
+swap(data,(lo + rng.nextInt(hi-lo)),lo);
+while (lo<hi)
+  if(data[lo+1] < data[st]) lo++;
+  else swap(data,lo+1,hi--);
+swap(data,lo,st);
+return lo;
 }
 public static void quicksort(int[] data){
   int lo = 0;
@@ -47,7 +39,7 @@ public static void quicksort(int[] data){
 }
 public static void quicksort(int[] data, int s, int e){
   if(s >= e) return;
-  int i = partitionDutch(data,s,e);
+  int i = partition(data,s,e);
   quicksort(data,i+1,e);
   quicksort(data,s,i);
 }
@@ -66,20 +58,68 @@ public static int quickselect(int []data, int k){
   int lo = 0;
   int hi = data.length-1;
   while(true){
-    int i = partitionDutch(data,lo,hi);
+    int i = partition(data,lo,hi);
   if(k == i) return data[k];
   if(k < i) hi = i-1;
   else lo = i+1;
 }
 }
 public static void main(String[] args){
-  int[] nums =
-    arrgen(1000000);
 
-  int[] nums2 = nums.clone();
+  int[] nums = {
+   2,2,3,3,1,1,-2,-2
+  };
+  int[] nums2 = {
+   2,2,3,3,1,1,-2,-2
+  };
+  quicksort(nums);
+  Arrays.sort(nums2);
+  if(Arrays.equals(nums,nums2)) System.out.println("repeating works");
+  nums = new int[] {
+   1,2,3,4,5,6,7
+  };
+    nums2 = new int[] {
+  1,2,3,4,5,6,7
+  };
+  quicksort(nums);
+  Arrays.sort(nums2);
+  if(Arrays.equals(nums,nums2)) System.out.println("presorted works");
+  nums = new int[] {
+   0,0,0,000,0
+  };
+  nums2 = new int[] {
+   0000,0,0,00,0
+  };
+  quicksort(nums);
+  Arrays.sort(nums2);
+  if(Arrays.equals(nums,nums2)) System.out.println("0s work");
+  nums = new int[] {
+   5,4,3,2,1
+  };
+  nums2 = new int[] {
+   5,4,3,2,1
+  };
+  quicksort(nums);
+  Arrays.sort(nums2);
+  if(Arrays.equals(nums,nums2)) System.out.println("reverse works");
+  nums =
+    arrgen(5000000);
+
+  nums2 = nums.clone();
   quicksort(nums);
   Arrays.sort(nums2);
   if(Arrays.equals(nums,nums2)) System.out.println("Big Random Array Works");
+  nums = new int[] {
+   6,7,89,4,2,4
+  };
+  nums2 = new int[] {
+    6,7,89,4,2,4
+  };
+  quicksort(nums);
+  Arrays.sort(nums2);
+
+  if(Arrays.equals(nums,nums2)) System.out.println("Test Array works");
+
 
 }
 
