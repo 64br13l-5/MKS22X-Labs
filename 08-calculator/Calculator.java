ea37315e06d00e4c390807b43fdc271bb9f4933a
Gabriel Small <gsmall30@stuy.edu>
2022-03-31 11:40:56 -0400

    Finished Calculator

---
+++ 08-calculator/Calculator.java
@@ -0,0 +1,39 @@
+  import java.util.*;
+    public class Calculator{
+      /*Evaluate a postfix expression stored in s.
+      *Assume valid postfix notation, of ints doubles and operators separated by spaces.
+      *Valid operators are + - / * and % (remainder not modulo)
+      *All results are doubles even if the operands are both int.
+      *@throws IllegalArgumentException when there are too many or too few operands.
+      *        Use the string parameter of your exception to indicate what happened.
+      *        Either "too many operands" or "too few operands for operation _" replace _ with +,- etc.
+      */
+      public static double eval(String s){
+        Deque<Double> d = new  ArrayDeque<>();
+        Scanner input = new Scanner(s);
+        while(input.hasNext()) {
+          var x = input.next();
+          try{d.addLast(Double.parseDouble(x));}
+          catch(Exception e) {
+            if(d.size() < 2) throw new IllegalArgumentException("too many operators");
+            var a = d.removeLast();
+            var b = d.removeLast();
+            switch(x) {
+            case "*": d.addLast(b * a);
+                  break;
+            case "+": d.addLast(b + a);
+                  break;
+            case "/":d.addLast(b / a);
+                  break;
+              case "-":d.addLast(b - a);
+                  break;
+            }
+          }
+      }
+        if(d.size() > 1) throw new IllegalArgumentException("not enough operators");
+        return d.getLast();
+      }
+      public static void main(String[] args){
+        System.out.println(eval("13 20 2 * +"));
+      }
+    }
