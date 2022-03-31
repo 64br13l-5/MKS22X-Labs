92b49bff284e548d5369f92d6bb63ca7d8cc8103
Gabriel Small <gsmall30@stuy.edu>
2022-03-31 11:43:43 -0400

    tested Calculator

--- 08-calculator/Calculator.java
+++ 08-calculator/Calculator.java
@@ -34,6 +34,11 @@
         return d.getLast();
       }
       public static void main(String[] args){
-        System.out.println(eval("13 20 2 * +"));
+
+        System.out.println(eval("11 3 - 4 + 2.5 *"));
+        System.out.println(eval("10 2.0 +"));
+        System.out.println(eval("8 2 + 99 9 - * 2 + 9 -")) ;
+            System.out.println(eval("1 2 3 4 5 + * - -"));
+            System.out.println(eval("1 2 3 4 5 + * - - -"));
       }
     }
